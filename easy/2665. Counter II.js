a3af46dbaa7b08ee92f55bf69e5a8942af43be55
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2025-03-01 21:25:21 +0200

    feat: solve 2665. Counter II

---
+++ easy/2665. Counter II.js
@@ -0,0 +1,49 @@
+//2665. Counter II
+//Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+//The three functions are:
+//increment() increases the current value by 1 and then returns it.
+//decrement() reduces the current value by 1 and then returns it.
+//reset() sets the current value to init and then returns it.
+
+///////////////////////////////////////////////////////////////////////////////
+
+//My solution:
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+  let currentValue = init;
+
+  return {
+    increment: function () {
+      currentValue += 1;
+      return currentValue;
+    },
+    decrement: function () {
+      currentValue -= 1;
+      return currentValue;
+    },
+    reset: function () {
+      currentValue = init;
+      return currentValue;
+    },
+  };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
+
+//Best solution:
+var createCounter = function (init) {
+  let n = init;
+  return {
+    increment: () => ++n,
+    reset: () => (n = init),
+    decrement: () => --n,
+  };
+};

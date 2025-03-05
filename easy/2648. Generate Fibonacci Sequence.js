d6431c64870bfa8f4c8aa51d862972e1caca8352
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2025-03-05 22:06:55 +0700

    feat: solve 2648. Generate Fibonacci Sequence

---
+++ easy/2648. Generate Fibonacci Sequence.js
@@ -0,0 +1,25 @@
+//2648. Generate Fibonacci Sequence
+//Write a generator function that returns a generator object which yields the fibonacci sequence.
+//The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
+//The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13.
+
+/////////////////////////////////////////////////////////////////////////////////
+
+//My and only solution:
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function* () {
+  let current = 0;
+  let next = 1;
+  while (true) {
+    yield current;
+    [current, next] = [next, current + next];
+  }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */

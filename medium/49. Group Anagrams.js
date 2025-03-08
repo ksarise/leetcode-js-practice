e12ed03a274250f68cb121cae345f3e612db93b3
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2025-03-08 23:46:28 +0700

    feat: solve 49. Group Anagrams

---
+++ medium/49. Group Anagrams.js
@@ -0,0 +1,23 @@
+//49. Group Anagrams
+//Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+////////////////////////////////////////////////////////////////////////////////
+
+//My and only solution:
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  const map = new Map();
+
+  for (const str of strs) {
+    const sorted = str.split("").sort().join("");
+    if (!map.has(sorted)) {
+      map.set(sorted, []);
+    }
+    map.get(sorted).push(str);
+  }
+
+  return Array.from(map.values());
+};
